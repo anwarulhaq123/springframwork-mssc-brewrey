f8e82f667aa6dffd97706937e4ba9af02db8fd31
Muhammad Anwar Ul Haq <48963489+anwarulhaq123@users.noreply.github.com>
2021-07-04 23:51:16 +0100

    Add files via upload

---
+++ mssc-brewrey/src/test/java/anwar/springframework/msscbrewrey/MsscBrewreyApplicationTests.java
@@ -0,0 +1,13 @@
+package anwar.springframework.msscbrewrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewreyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
